d90c2bf2849a08c6e508f8b65ba95d1c5cad6e1b
xxyury <med.yuryxx@gmail.com>
2025-10-13 21:38:23 +0200

    test2

---
+++ test test/test2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "pew pew!" << std::endl;
+    return 0;
+}
+
